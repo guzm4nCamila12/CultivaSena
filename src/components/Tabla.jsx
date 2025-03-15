9511b4e5dd50c3bcdf891072f376983d1c031618
guzm4nCamila12 <mariaguramos12@gmail.com>
2025-03-15 16:52:53 -0500

    boton agregar bien

--- src/components/Tabla.jsx
+++ src/components/Tabla.jsx
@@ -41,7 +41,7 @@ const Tabla = ({ columnas, datos, titulo, acciones, onAddUser, mostrarAgregar })
    
             {/* Contenedor del título y el buscador */}
             <div className="flex flex-col sm:flex-row justify-between items-center mb-4 gap-3">
-                <h1 className="text-[27px] font-medium">{titulo}</h1>
+                <h1 className="text-2xl font-semibold ml-3">{titulo}</h1>
                 <div className="relative flex items-center w-full sm:w-80 bg-gray-100 rounded-full border border-gray-300">
                     <img src={search} alt="Buscar" className="absolute left-3" />
                     <input

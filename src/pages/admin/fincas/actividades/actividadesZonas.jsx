06fbb269daa1e233b5303ee6bb6216f120e30c3a
JuanesRamirez <estebanjuan421@gmail.com>
2025-03-27 10:54:30 -0500

    Acciones de Actividad

--- src/pages/admin/fincas/actividades/actividadesZonas.jsx
+++ src/pages/admin/fincas/actividades/actividadesZonas.jsx
@@ -4,6 +4,9 @@ import { useParams } from 'react-router-dom'
 import MostrarInfo from '../../../../components/mostrarInfo';
 import Navbar from '../../../../components/navbar';
 
+//Iconos para acciones
+import verTodo from '../../../../assets/icons/sinFincas.png'
+import eliminarIcon from '../../../../assets/icons/deleteWhite.png'
 
 
 function ActividadesZonas() {
@@ -14,7 +17,9 @@ function ActividadesZonas() {
 
     useEffect(() => {
         getActividadesByZona(id)
-            .then((data) => {setActividades(data)
+            .then((data) => {
+                console.log(data)
+                setActividades(data)
         })
         getZonasById(id)
         .then((data) => {
@@ -24,10 +29,36 @@ function ActividadesZonas() {
     },[id]);
 
     const columnas = [
-        { key: "nombre", label: "Actividad"},
         { key: "cultivo", label: "Cultivo" },
         { key: "etapa", label: "Etapa" },
     ];
+
+    const acciones = (fila) => (
+        <div className="flex justify-center gap-2">
+          <div className="relative group">
+            <button
+              className="xl:px-8 px-5 py-2 rounded-full bg-[#00304D] hover:bg-[#002438] flex items-center justify-center transition-all"
+            //   onClick={() => HandleAgregarActividad(fila.id)}
+            >
+              <img src={verTodo} alt="Agregar Actividad" className="w-5 h-5" />
+            </button>
+            <span className="absolute left-1/2 -translate-x-1/2 bottom-full mb-1 text-xs bg-gray-700 text-white px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none">
+              Ver Todo
+            </span>
+          </div>
+          <div className="relative group">
+            <button
+              className="xl:px-8 px-5 py-2 rounded-full bg-[#00304D] hover:bg-[#002438] flex items-center justify-center transition-all"
+            //   onClick={() => HandleEditarZona(fila)}
+            >
+              <img src={eliminarIcon} alt="Eliminar" />
+            </button>
+            <span className="absolute left-1/2 -translate-x-1/2 bottom-full mb-1 text-xs bg-gray-700 text-white px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none">
+              Eliminar
+            </span>
+          </div>
+        </div>
+      );
 
 
     return (
@@ -37,8 +68,8 @@ function ActividadesZonas() {
                 titulo={`Actividades de: ${zonas.nombre}`}
                 columnas={columnas}
                 datos={Array.isArray(actividades) ? actividades : []}
+                acciones={acciones}
                 mostrarAgregar={true}
-
             />
         </div>
     )

b462977b3d94b998601fd54af2f4f0d54e183dd6
guzm4nCamila12 <mariaguramos12@gmail.com>
2025-04-22 09:46:38 -0500

    menu y descripcion

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -44,18 +44,18 @@ export default function Navbar() {
           <BotonAtras />
           {/* Menu lateral*/}
           <div
-            className={`absolute mt-[172px] top-10 w-64 bg-[#002A43] p-4 rounded-xl shadow-lg z-50 transition-opacity duration-300 ease-in-out ${menuVisible ? 'opacity-100 visible' : 'opacity-0 invisible'}`}>
+            className={`absolute mt-[172px] top-10 w-60 bg-[#002A43] p-3 rounded-xl shadow-lg z-50 transition-opacity duration-300 ease-in-out ${menuVisible ? 'opacity-100 visible' : 'opacity-0 invisible'}`}>
             {/* Div con forma triangular*/}
             <div
               className="absolute -top-2 xl:left-4 transform -translate-x-1/2 w-0 h-0 border-l-[5px] border-r-[5px] border-b-[9px] border-l-transparent border-r-transparent border-b-[#00304D]">
             </div>
             <div>
-              <h3 className='w-full flex mb-3 text-white font-medium  border-b-2 pb-2 pl-2 border-white'>Menú Principal</h3>
-              <div className='flex ml-3 pt-2 text-white'>
+              <h3 className='w-full flex mb-2 text-white font-medium border-b-2 pb-1 pl-12 border-white'>Menú Principal</h3>
+              <div className='flex ml-3 pt-1 text-white'>
                 <img src={cultivaSena} alt="" className='w-5 h-5 mr-2' />
                 <h4>Ir a CultivaSena</h4>
               </div>
-              <div className='flex rounded-3xl mt-2'>
+              <div className='flex rounded-3xl '>
                 {/*Llamamos el componente que tiene la funcionalidad de cerrar sesion */}
                 <CerrarSesion />
               </div>

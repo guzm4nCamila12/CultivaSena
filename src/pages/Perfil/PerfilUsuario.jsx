d9543e90069a6092ee1cf2dce658bd3e3842c71b
JuanesRamirez <estebanjuan421@gmail.com>
2025-06-10 13:53:16 -0500

    Cambios en estilos perfil y responsive

--- src/pages/Perfil/PerfilUsuario.jsx
+++ src/pages/Perfil/PerfilUsuario.jsx
@@ -4,14 +4,13 @@ import { useNavigate, Link } from 'react-router-dom'
 //Componentes reutilizados
 import Navbar from '../../components/navbar'
 import Tabla from '../../components/Tabla'
-import FormularioModal from '../../components/modals/FormularioModal'
 import { acctionSucessful } from '../../components/alertSuccesful'
 //Importacion de icono e imagenes
 import {
-  fincasIcon, nombreIcon, sensoresIcon, editar, usuarioAzul, correoAzul, telefonoAzul, ajustes, actividadesIcon, fecha as fechaIcon, ver, zonasIcon,
-  sensoresTarjeta, fincaTarjeta as fincaTarjetaIcon, zonaTarjeta, usuarioTarjeta as usuarioTarjetaIcon
+  fincasIcon, sensoresIcon, editar, ajustes, actividadesIcon, fecha as fechaIcon, ver,
+  sensoresTarjeta, fincaTarjeta as fincaTarjetaIcon, zonaTarjeta, usuarioTarjeta as usuarioTarjetaIcon,
 } from '../../assets/icons/IconsExportation'
-import { fincaPerfil, finca, usuarioCreado, usuarioTarjeta, vacaTarjeta, fincaTarjeta } from '../../assets/img/imagesExportation'
+import { fincaPerfil, usuarioCreado, usuarioTarjeta, vacaTarjeta, fincaTarjeta } from '../../assets/img/imagesExportation'
 //Endpoints para consumir el api
 import { getCantidadSensores, getCountSensoresByFinca } from '../../services/sensores/ApiSensores'
 import { getUsuarioById, getHistorial } from '../../services/usuarios/ApiUsuarios'
@@ -19,6 +18,7 @@ import { getActividadesTotales, getActividadesByUsuario, getZonasById, getCountF
 //Hooks
 import { useUsuarios } from '../../hooks/useUsuarios'
 import { obtenerIdUsuario, obtenerRol } from '../../hooks/useDecodeToken'
+import { editarUsuario } from '../../services/usuarios/ApiUsuarios'
 
 function PerfilUsuario() {
   const navigate = useNavigate()
@@ -27,7 +27,6 @@ function PerfilUsuario() {
   const [cantidadZonas, setCantidadZonas] = useState({})
   const [cantidadSensoresFinca, setCantidadSensoresFinca] = useState({})
   const [usuario, setUsuario] = useState({})
-  const [modalEditarAbierto, setModalEditarAbierto] = useState(false)
   const [usuarioEditar, setUsuarioEditar] = useState({})
   const { actualizarUsuario } = useUsuarios()
   const [usuarioOriginal, setUsuarioOriginal] = useState(null)
@@ -37,6 +36,8 @@ function PerfilUsuario() {
   // Estados para el modal de historial completo (rol 1)
   const [modalHistorialAbierto, setModalHistorialAbierto] = useState(false)
   const [historialSeleccionado, setHistorialSeleccionado] = useState(null)
+
+  const [modoEdicion, setModoEdicion] = useState(false);
 
   // Formatea ISO a 'DD/MM/YYYY H:mm' usando hora local
   const formatFecha = (iso) => {
@@ -70,6 +71,7 @@ function PerfilUsuario() {
           .then(data => {
             setUsuario(data)
             setUsuarioEditar(data)
+            setUsuarioOriginal(data)
           })
       } catch (err) {
         console.error("Error cargando sensores o usuario", err)
@@ -200,12 +202,6 @@ function PerfilUsuario() {
     }
   }
 
-  const abrirModalEditar = (u) => {
-    setUsuarioEditar({ ...u })
-    setUsuarioOriginal({ ...u })
-    setModalEditarAbierto(true)
-  }
-
   const handleEditarUsuario = async (e) => {
     e.preventDefault()
     const exito = await actualizarUsuario(usuarioEditar, usuarioOriginal)
@@ -216,10 +212,8 @@ function PerfilUsuario() {
         title: `¡Tu información se ha editado con éxito!`
       })
       setUsuario(usuarioEditar)
-      setModalEditarAbierto(false)
     }
   }
-
   // Acciones según rol:
   // - Rol 1: abre modal historial
   // - Rol 2: redirige a actividadesZonas con idzona
@@ -267,22 +261,22 @@ function PerfilUsuario() {
   const columnas = obtenerRol() === 1
     ? [
       { key: "operacion", label: "Operación", icon2: ajustes },
-      { key: "tabla", label: "Tabla", icon2: ajustes },
+      { key: "tabla", label: "Tabla", icon2: actividadesIcon },
       { key: "fecha", label: "Fecha", icon2: fechaIcon },
-      { key: "acciones", label: "Acciones", icon2: ajustes }
+      { key: "acciones", label: "Ver", icon2: ajustes }
     ]
     : obtenerRol() === 2
       ? [
         { key: "finca_nombre", label: "Finca", icon2: fincasIcon },
         { key: "actividad", label: "Actividad", icon2: actividadesIcon },
         { key: "fechafin", label: "Fecha", icon2: fechaIcon },
-        { key: "acciones", label: "Acciones", icon2: ajustes }
+        { key: "acciones", label: "Ver", icon2: ajustes }
       ]
       : [
         { key: "zona", label: "Zona", icon2: actividadesIcon },
         { key: "actividad", label: "Actividad", icon2: actividadesIcon },
         { key: "fechafin", label: "Fecha", icon2: fechaIcon },
-        { key: "acciones", label: "Acciones", icon2: ajustes }
+        { key: "acciones", label: "Ver", icon2: ajustes }
       ]
 
   const ruta =
@@ -295,24 +289,26 @@ function PerfilUsuario() {
           : '/'
 
   return (
-    <div className="bg-orange-500 flex flex-col h-screen">
+    <div>
       <Navbar />
-      <div className="flex h-full justify-end bg-purple-500 container mx-auto">
-
-        <div className=" flex w-full h-full">
-          <div className=" w-[32rem] flex flex-col items-center bg-white my-10 rounded-3xl ">
-            <div className=" mt-9  flex justify-center items-center bg-[#00304D] rounded-full">
-              <img src={fincaPerfil} alt="" className="w-56 h-56" />
+      <div className="flex lg:flex lg:flex-wrap lg:justify-center justify-end container mx-auto">
+        <div className="sm:flex sm:flex-col xl:flex-row xl:flex xl:justify-between w-full xl:h-full">
+          <div className=" 2xl:w-[32rem] xl:p-0 2xl:px-[1rem] sm:px-[2rem] xl:w-[23rem] sm:justify-center sm:flex-row flex xl:flex-col items-center xl:px-[0.5rem] mt-[2rem] bg-white xl:my-10 rounded-3xl ">
+            <div className="mt-9 p-2 xl:mt-0 flex justify-center items-center sm:my-[3rem] xl:my-0 bg-[#00304D] rounded-full">
+              <img src={fincaPerfil} alt="" className="w-auto sm:w-auto sm:h-auto h-auto" />
             </div>
-            <div className="w-full px-4  mt-12 space-y-4 flex flex-col justify-end ">
+            <div className="w-full sm:px-[4rem] xl: sm:my-[3rem] xl:p-0 space-y-1 sm:space-y-4 flex flex-col justify-end ">
               {/* Nombre */}
               <div>
                 <label className="block text-gray-600 mb-1">Nombre</label>
                 <div className="relative">
                   <input
                     type="text"
-                    readOnly
-                    value={usuario.nombre || ""}
+                    readOnly={!modoEdicion}
+                    value={modoEdicion ? usuarioEditar.nombre || "" : usuario.nombre || ""}
+                    onChange={(e) =>
+                      setUsuarioEditar({ ...usuarioEditar, nombre: e.target.value })
+                    }
                     className="w-full py-2 pl-3 pr-4 bg-[#EEEEEE] font-bold rounded-full border border-transparent focus:outline-none focus:ring-2 focus:ring-[#39A900] h-12"
                   />
                 </div>
@@ -320,13 +316,16 @@ function PerfilUsuario() {
 
               {/* Teléfono */}
               <div>
-                <label className="block text-gray-600 mb-1">Número de contacto</label>
+                <label className="block  text-gray-600 mb-1">Número de contacto</label>
                 <div className="relative">
 
                   <input
                     type="text"
-                    readOnly
-                    value={usuario.telefono || ""}
+                    readOnly={!modoEdicion}
+                    value={modoEdicion ? usuarioEditar.telefono || "" : usuario.telefono || ""}
+                    onChange={(e) =>
+                      setUsuarioEditar({ ...usuarioEditar, telefono: e.target.value})
+                    }
                     className="w-full py-2 pl-3 pr-4 bg-[#EEEEEE] font-bold rounded-full border border-transparent focus:outline-none focus:ring-2 focus:ring-[#39A900] h-12"
                   />
                 </div>
@@ -336,40 +335,66 @@ function PerfilUsuario() {
               <div>
                 <label className="block text-gray-600 mb-1">Correo</label>
                 <div className="relative">
-                
+
                   <input
                     type="email"
-                    readOnly
-                    value={usuario.correo || ""}
+                    readOnly={!modoEdicion}
+                    value={modoEdicion ? usuarioEditar.correo || "" : usuario.correo || ""}
+                    onChange={(e) =>
+                      setUsuarioEditar({ ...usuarioEditar, correo: e.target.value})
+                    }
                     className="w-full py-2 pl-3 pr-4 bg-[#EEEEEE] font-bold rounded-full border focus:outline-none focus:ring-2 focus:ring-[#39A900] h-12"
                   />
                 </div>
               </div>
 
               {/* Botón editar */}
-              <button
-                className="flex items-center justify-center w-full bg-[#39A900] hover:bg-[#005F00] transition py-2 rounded-full text-white font-semibold h-12"
-                onClick={() => abrirModalEditar(usuario)}
-              >
-                <img src={editar} alt="editar" className="w-5 h-5 mr-2" />
-                Actualizar Datos
-              </button>
+              {!modoEdicion ? (
+                <button
+                  className="flex items-center justify-center w-full bg-[#39A900] hover:bg-[#005F00] transition py-2 rounded-full text-white font-semibold h-12"
+                  onClick={() => setModoEdicion(true)}
+                >
+                  <img src={editar} alt="editar" className="w-5 h-5 mr-2" />
+                  Actualizar Datos
+                </button>
+              ) : (
+                <div className="flex w-full gap-4">
+                  <button
+                    className="w-1/2 bg-[#002A43] hover:bg-[#021926] text-white font-semibold py-2 rounded-full h-12"
+                    onClick={() => {
+                      setModoEdicion(false);
+                      setUsuarioEditar({ ...usuario });
+                      setUsuarioOriginal({ ...usuario }); // opcional, para poder restaurar
+                    }}
+                  >
+                    Cancelar
+                  </button>
+                  <button
+                    className="w-1/2 bg-[#39A900] hover:bg-[#005F00] text-white font-semibold py-2 rounded-full h-12"
+                    onClick={(e) => {
+                      handleEditarUsuario(e)
+                      setModoEdicion(false);
+                    }}
+                  >
+                    Guardar
+                  </button>
+                </div>
+              )}
             </div>
-
           </div>
 
           {/* Contenedor de cartas: cantidad fincas y cantidad sensores */}
-          <div className=" md:space-y-0 md:justify-center md:gap-24 lg:gap-36 2xl:gap-32 lg:w-1/4  md:flex-row lg:flex lg:flex-col  flex flex-col gap-6 items-center">
+          <div className=" mt-[2.5rem] flex justify-between sm:h-full sm:justify-between sm:flex-row sm:flex sm:w-full xl:gap-[12rem] 2xl:gap-[12rem]  xl:flex xl:flex-col xl:justify-around xl:w-auto xl:h-full">
             <div
               onClick={() => navigate(ruta)}
-              className="bg-cover h-[37%] border-4 text-lg rounded-full flex flex-wrap sm:mt-0 md:mt-0 text-white w-full sm:rounded-3xl sm:max-w-xs p-5 shadow-lg hover:scale-95 transition cursor-pointer "
+              className="bg-cover hover:scale-95 transition xl:w-full w-2/5 h-[16.5rem] border-4 text-lg rounded-3xl flex flex-wrap sm:mt-0 md:mt-0 text-white sm:rounded-3xl sm:max-w-xs p-5 shadow-lg cursor-pointer "
               style={{ backgroundImage: `url(${cartas("tarjeta")})` }}
             >
               <div className=' font-bold text-4xl w-2/3 h-1/2'>
                 <h3>{cartas("texto")}</h3>
               </div>
               <div className=" flex items-start justify-end w-1/3 h-1/2 p-1">
-                <img src={cartas("imagen")} alt="Icono" className="h-14" />
+                <img src={cartas("imagen")} alt="Icono" className="hidden md:block md:h-14" />
               </div>
 
               <div className="text-8xl w-full font-bold flex items-end mt-3 justify-end h-1/2 p-1">
@@ -382,17 +407,17 @@ function PerfilUsuario() {
                 ) : null}
               </div>
             </div>
-                
+
             <div
               onClick={() => navigate(ruta)}
-              className="bg-cover h-[37%] border-4 text-lg rounded-full flex flex-wrap sm:mt-0 md:mt-0 text-white w-full sm:rounded-3xl sm:max-w-xs p-5  shadow-lg hover:scale-95 transition cursor-pointer"
+              className="bg-cover hover:scale-95 transition xl:w-full h-[16.5rem] border-4 text-lg rounded-3xl flex flex-wrap sm:mt-0 md:mt-0 text-white w-2/5 sm:rounded-3xl sm:max-w-xs p-5  shadow-lg cursor-pointer"
               style={{ backgroundImage: `url(${cartas("tarjeta2")})` }}
             >
               <div className='font-bold text-4xl w-2/3 h-1/2'>
                 <h3>{cartas("texto2")}</h3>
               </div>
               <div className="flex items-start justify-end  w-1/3 h-1/2 p-1 ">
-                <img src={cartas("imagen2")} alt="Icono" className="h-16 " />
+                <img src={cartas("imagen2")} alt="Icono" className="md:block md:h-14 hidden " />
               </div>
 
               <div className="text-8xl w-full font-bold flex items-end mt-3 justify-end  h-1/2 p-1">
@@ -409,8 +434,8 @@ function PerfilUsuario() {
           </div>
 
           {/* Contenedor tabla actividades / historial */}
-          <div className=" flex flex-col py-7 items-start w-2/3">
-            <div className="bg-[#002A43] w-4/5 shadow-slate-700 shadow-lg mt-3 mb-3 h-full rounded-3xl flex flex-col items-center p-4">
+          <div className=" sm:w-full flex flex-col py-7 items-end xl:w-2/5">
+            <div className="bg-[#002A43]  w-full xl:w-full shadow-slate-700 shadow-lg mt-3 mb-3 h-full rounded-3xl flex flex-col items-center p-4">
               <h3 className="font-bold text-xl mt-1 text-white">
                 {obtenerRol() === 1 ? 'Historial' : obtenerRol() === 2 ? 'Registro Actividades' : "Actividades Realizadas"}
               </h3>
@@ -452,24 +477,6 @@ function PerfilUsuario() {
             </button>
           </div>
         </div>
-      )}
-
-      {/* Modal para editar usuario */}
-      {usuarioEditar && (
-        <FormularioModal
-          titulo="Editar Información"
-          isOpen={modalEditarAbierto}
-          onClose={() => setModalEditarAbierto(false)}
-          onSubmit={handleEditarUsuario}
-          valores={usuarioEditar}
-          onChange={(e) => setUsuarioEditar({ ...usuarioEditar, [e.target.name]: e.target.value })}
-          textoBoton="Guardar y actualizar"
-          campos={[
-            { name: "nombre", placeholder: "Nombre", icono: usuarioAzul },
-            { name: "telefono", placeholder: "Teléfono", icono: telefonoAzul },
-            { name: "correo", placeholder: "Correo", icono: correoAzul }
-          ]}
-        />
       )}
     </div>
   )

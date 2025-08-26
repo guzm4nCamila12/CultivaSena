21e57a68d80b21a0afe4cc7a59deed7b0226d15a
juan hernandez <juancanas651@gmail.com>
2025-08-26 12:42:09 -0500

    validaciones login

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -22,7 +22,14 @@ export function useLogin() {
 
     const iniciarSesion = async (e) => {
         e.preventDefault()
-
+        if (!usuario.telefono || !usuario.clave) {
+            acctionSucessful.fire({
+                imageUrl: estadoRechazado,
+                imageAlt: "Icono de error",
+                title: `Complete todos los campos`
+            })
+            return
+        }
         try {
             const resultado = await login(usuario);
             if (resultado && resultado.token) {
